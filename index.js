253176cd29e2ef8781cf0d3cdb6bbfedd8c43993
MarkTocino <Mark_tocino@yahoo.com>
2023-06-29 16:49:33 -0400

    Only CSS Needed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+let currentFood;
 
 fetch('http://localhost:3000/Food')
 .then(resp => resp.json())
 .then(data => {
     data.forEach(food => foodList (food))
 displayFood(data[0])
+addLikes ()
+newComment()
 });
 function foodList (food) {
     const createImage = document.createElement("img");
@@ -12,15 +15,22 @@ function foodList (food) {
     const burgerList = document.getElementById("burger-list");
     pTag.innerText = food.name;
     createImage.src = food.images;
+    createImage.style.filter= "grayscale(100%)"
     createDiv.append(createImage)
     burgerList.append(createDiv);
     createDiv.append(pTag);
+    createImage.addEventListener("mouseover", () => {
+        createImage.style.filter = "grayscale(0%)"
+    })
+    createImage.addEventListener("mouseout", () => {
+        createImage.style.filter = "grayscale(100%)"
+    })
     createImage.addEventListener("click", () => {
         displayFood(food)
     })
-    };
+};
 function displayFood(food) {
-
+    currentFood = food
     const burgerImages = document.getElementById("burger-images")
     burgerImages.src = food.images
 
@@ -31,10 +41,28 @@ function displayFood(food) {
     description.textContent = `Description: ${food.description}`
     
     const comments = document.getElementById("comments")
-    comments.textContent = `Comments: ${food.comments}`
+    comments.textContent = `Comment: ${food.comments}`
     
 
     const rating = document.getElementById("rating")
     rating.textContent = `Rating: ${food.rating}` 
-};
 
+};
+function addLikes () {
+    const button = document.getElementById("btn");
+    button.addEventListener("click", () => {
+    currentFood.rating ++
+    rating.textContent = `Rating: ${currentFood.rating}`
+})
+};
+function newComment () {
+    const createComment = document.getElementById("createComment");
+createComment.addEventListener("submit", e => {
+e.preventDefault()
+    const newComment = document.getElementById("new-comment");
+    const comment = document.getElementById("comments")
+    const div = document.createElement("div")
+    comment.append(div)
+    div.textContent = `Comment: ${newComment.value}`
+    })
+};
